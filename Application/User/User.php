fe5f1f7f1a501aa479316fc8fad078b0db0986bd
Gary Saunders <gary@powrit.com>
2014-09-18 10:21:35 -0400

    Moving the User model into the Application namespace, configuring auth to use it.

---
+++ Application/User/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\User;
+
+use \Illuminate\Auth\UserTrait;
+use \Illuminate\Auth\UserInterface;
+use \Illuminate\Auth\Reminders\RemindableTrait;
+use \Illuminate\Auth\Reminders\RemindableInterface;
+use \Illuminate\Database\Eloquent\Model;
+
+class User extends Model implements UserInterface, RemindableInterface {
+
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password', 'remember_token');
+
+}
